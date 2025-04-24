a1ae84b68de016cfa2423aa2708a9cf803c69d42
Tela634 <veronicawaithira680@gmail.com>
2025-04-24 08:12:20 +0300

    settings.js

---
+++ settings.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+//Feel free to customize my bot informations to yours. Starting from this point
+//owmner v card
+global.ytname = "YT: ElitePro" //ur yt chanel name
+global.socialm = "GitHub: Toronto-Tec" //ur github or insta name
+global.location = "Kenya, East Africa" //ur location
+
+//new settings 
+global.botname = '⌐⁠■✧Lesta_Bot✧■→' //ur bot name
+global.ownernumber = '254701309409' //ur owner number
+global.ownername = 'Eliud' //ur owner name
+global.websitex = "https://youtube.com/@elitepro-empire"
+global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
+
+global.themeemoji = '😁'
+global.wm = "ELIUD"
+global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
+global.packname = "Sticker By Lesta"
+global.author = "LESTA_BOT\n\n+254701309409"
+global.creator = "254701309409@s.whatsapp.net"
+global.xprefix = '.' //set any prefix 
+global.premium = ["254701309409"] // Premium User
+//Stop here. Thanks for choosing Lesta_Pro.
+
+
+//bot settings 
+global.hituet = 0
+global.typemenu = 'v1'
+global.typereply = 'v4' 
+global.autoblocknumber = '92' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+global.welcome = true //welcome/left in groups
+global.anticall = true //bot blocks user when called
+global.autoswview = true //auto status/story view
+global.adminevent = true //show promote/demote message
+global.groupevent = true //show update messages in group chat
+
+//Global messages
+global.mess = {
+        limit: 'Your limit is up!',
+        nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+    done: 'Done✓',
+    error: 'Error!',
+    success: 'Here you go!'
+}
+
+
+//text bug
+global.xbugtex = {
+xtxt: '🍞LESTA-BOT🎀',
+}
